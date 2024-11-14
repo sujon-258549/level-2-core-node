403064380729a4ac6066c718b03b264eb965c2ff
Md Sujon Mia <sujon258549@gmail.com>
2024-11-14 22:43:58 +0600

    modified asinc-file.js added write.text

--- asinc-file.js
+++ asinc-file.js
@@ -5,6 +5,11 @@ fs.readFile('./texts/read.text', 'utf-8',(error, data) => {
     if(error) {
         throw new Error('error reding text')
     }
+    fs.writeFile('./texts/write.text', data, 'utf-8',(error) =>{
+        if(error){
+            throw Error('vai moi nai')
+        }
+    })
     console.log(data)
 })
 
