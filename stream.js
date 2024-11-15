bcadd7613efa79d8b6e6b681dc2d3947ba69a183
Md Sujon Mia <sujon258549@gmail.com>
2024-11-15 10:59:21 +0600

    modified stream.js and read.text

--- stream.js
+++ stream.js
@@ -1,15 +1,54 @@
-const http = require('http')
-
+const http = require('http');
+const fs = require('fs');
 
 //  create local servire 
-// 
-const servire = http.createServer()
+const server = http.createServer()
 
-servire.on('request', (req, res) => {
-    console.log(req.url, req.method);
-    res.end('hello servire Howare You')
+server.on('request', (req, res) => {
+    if (req.url === "/read-file"&& req.method === "GET") {
+        const stream = fs.createReadStream(process.cwd() + '/texts/read.text')
+        stream.on('data', (buffer) => {
+            res.write(buffer)
+        })
+        stream.on('end', () => {
+
+            res.end('hello servire Howare You')
+        })
+    };
+    // Create a stream from some character device.
+
 })
 
-servire.listen(3000, () => {
+server.listen(3000, () => {
     console.log(' servire is raning')
 })
+
+
+
+
+
+
+//         // Create a stream to read the file
+//         const stream = fs.createReadStream(process.cwd() + '/texts/read.text');
+
+//         stream.on('data', (chunk) => {
+//             res.write(chunk);
+//         });
+
+//         stream.on('end', () => {
+//             res.end(); // End the response when the file has been completely read
+//         });
+
+//         stream.on('error', (error) => {
+//             res.writeHead(500, { 'Content-Type': 'text/plain' });
+//             res.end('Error reading file');
+//         });
+//     } else {
+//         res.writeHead(404, { 'Content-Type': 'text/plain' });
+//         res.end('Not Found');
+//     }
+// });
+
+// server.listen(3000, () => {
+//     console.log('Server is running on port 3000');
+// });
